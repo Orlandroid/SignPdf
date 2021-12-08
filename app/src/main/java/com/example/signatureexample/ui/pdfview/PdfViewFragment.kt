993d7bf03b5794d27fb2671bb8c361c26bde9faf
Orlandroid <lopez.orlando.gold9@gmail.com>
2021-12-08 13:29:43 -0600

    Se agrego libreria para convertir el html to pdf

--- app/src/main/java/com/example/signatureexample/ui/pdfview/PdfViewFragment.kt
+++ app/src/main/java/com/example/signatureexample/ui/pdfview/PdfViewFragment.kt
@@ -2,12 +2,16 @@ package com.example.signatureexample.ui.pdfview
 
 import android.os.Bundle
 import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.signatureexample.R
 import com.example.signatureexample.databinding.FragmentPdfViewBinding
 import com.example.signatureexample.ui.util.Works
+import com.example.signatureexample.ui.util.getDirectoryPictures
 import com.example.signatureexample.ui.util.readXMLFileFromAsset
+import org.nvest.html_to_pdf.HtmlToPdfConvertor
+import java.io.File
 import java.lang.StringBuilder
 
 class PdfViewFragment : Fragment() {
@@ -15,11 +19,13 @@ class PdfViewFragment : Fragment() {
     private var _binding: FragmentPdfViewBinding? = null
     private val binding get() = _binding!!
     private var xmlStr: StringBuilder? = null
+    private lateinit var htmlToPdfConvertor: HtmlToPdfConvertor
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        htmlToPdfConvertor = HtmlToPdfConvertor(requireContext())
         _binding = FragmentPdfViewBinding.inflate(layoutInflater)
         return binding.root
     }
@@ -48,8 +54,25 @@ class PdfViewFragment : Fragment() {
             "utf-8",
             ""
         )
+        convertHtmlToPdf(xmlStr!!.toString())
     }
 
+    private fun convertHtmlToPdf(htmlString: String) {
+        val file = File(getDirectoryPictures(), "kotlin.pdf")
+        htmlToPdfConvertor.convert(
+            pdfLocation = file,
+            htmlString = htmlString,
+            onPdfGenerationFailed = { exception ->
+                exception.printStackTrace()
+            },
+            onPdfGenerated = { pdfFile ->
+                Toast.makeText(
+                    requireContext(),
+                    "Se genero correctamente el pdf$pdfFile",
+                    Toast.LENGTH_SHORT
+                ).show()
+            })
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setHasOptionsMenu(true)
